8b7063c2e56cede86564de951ab3df8ab24d1cc7
MS <manasa.ms@gmail.com>
2026-07-04 16:19:05 -0400

    Force dynamic rendering on dashboard

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,8 @@
 import { getAnalytics } from '@/lib/analytics'
 import Dashboard from '@/components/Dashboard'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const data = await getAnalytics()
   return <Dashboard data={data} />
